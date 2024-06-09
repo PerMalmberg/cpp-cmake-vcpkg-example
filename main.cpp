0b15abd97aeb7b044a01d9b805b83f8949150392
Per Malmberg <per.malmberg@gmail.com>
2024-06-09 19:03:01 +0200

    Add defaults to clang format and tidy.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include "other.h"
 
 int main(int argc, char **argv) {
-  fmt::print("Vim motions are hard.");
-  Other o{};
-  o.hello("foo bar");
+	fmt::print("Vim motions are hard.");
+	Other o{};
+	o.hello("foo bar");
 
-  return 0;
+	return 0;
+}
+
+void foo() {
+	return;
+	// Hello
 }
