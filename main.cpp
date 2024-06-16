3350fd26443039a9fbc356bf7d703c92d48f9538
Per Malmberg <per.malmberg@gmail.com>
2024-06-16 21:09:37 +0200

    Small changes to test clangd.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "fmt/core.h"
 #include "other.h"
+#include <iostream>
+#include <limits>
 
 int main(int argc, char **argv) {
 	fmt::print("Vim motions are hard.");
 	Other o{};
 	o.hello("foo bar");
-
+	std::cout << std::numeric_limits<std::uint64_t>::max();
 	return 0;
 }
 
--- other.cpp
+++ other.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 void Other::hello(const std::string &s) const {
-  std::cout << s;
-  std::cout << "Hello from other";
-  std::cout << "Hello from other";
+	std::cout << s;
+	std::cout << "Hello from other";
+	std::cout << "Hello from other";
+	std::cout << std::endl;
 }
